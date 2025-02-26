4c12523e30008e79b22b0ade1ec8f706fbd3447f
Aaditya Parmar <94757097+a1dtya@users.noreply.github.com>
2025-02-26 19:17:09 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+   async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/about-us',
+          permanent: true,
+        },
+      ]
+    }, 
 };
 
 export default nextConfig;
